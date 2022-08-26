25eff5574f87f6e273b0c74672fee34dfb29855c
abuzar7266 <i190531@nu.edu.pk>
2022-08-26 15:21:55 +0500

    React-Bootstrap Added

--- GDL-Visualisation/src/App.jsx
+++ GDL-Visualisation/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component{
     render()
     {
     return (<>  
-    <div style={{marginTop:0}}>
+    <div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
--- GDL-Visualisation/src/pages/Home.jsx
+++ GDL-Visualisation/src/pages/Home.jsx
@@ -1,8 +1,122 @@
 import React from "react";
+import { Container,Row,Col } from "react-bootstrap";
 const Test = ()=>{
-    return (<><div>
-        <h3>Hello World, to Home</h3><br />
-    </div>
+    return (<>
+    <Container fluid>
+    <Row style={{textAlign:'center'}}>
+        <Col>
+            <h1>Lorem Ipsum Dolor</h1>
+        </Col>
+    </Row>
+        <Row>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac laoreet nisl. Praesent id dignissim elit, non sollicitudin ante. Nullam efficitur turpis at urna imperdiet, eget tincidunt arcu varius. Nulla facilisi. Nulla pellentesque orci mauris, in mattis neque lacinia vel. Vestibulum malesuada risus ultricies est mattis, nec placerat enim sollicitudin. Quisque quis erat eu augue hendrerit varius. Pellentesque vitae consectetur lorem. Pellentesque at odio ac turpis venenatis egestas ac vitae eros. Vestibulum pellentesque laoreet luctus. Phasellus nec felis vitae diam varius pharetra vitae eget mauris. Nullam mollis erat lobortis nulla feugiat, vel feugiat velit bibendum. Sed turpis neque, dignissim sed interdum vel, gravida eu mauris. Nam dapibus elit tortor, quis elementum ex fringilla vel.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Vestibulum at tincidunt justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi in sagittis turpis, sed semper diam. Sed eu massa vitae tellus hendrerit iaculis vel vel augue. Pellentesque sed tortor sodales, suscipit lacus faucibus, faucibus tortor. Praesent quis scelerisque dui. Nunc tincidunt sem vitae sem finibus mollis. Nam at sapien sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis consectetur dapibus ligula, vel aliquet nibh consequat id. Nulla ac egestas leo, nec lacinia odio. Donec rhoncus est ac arcu gravida, volutpat congue dui rutrum. Sed a nunc ex. Aenean ullamcorper luctus varius. Aliquam mattis sem ut arcu laoreet, vel faucibus leo facilisis.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Cras ut finibus eros. Mauris arcu velit, iaculis in nibh vel, lacinia elementum nisl. Curabitur interdum arcu non enim hendrerit, sit amet placerat diam condimentum. Aliquam nec eros et ligula scelerisque bibendum. Nulla accumsan tincidunt elit cursus tincidunt. Sed ut dolor quis lorem hendrerit eleifend a sit amet nibh. Phasellus placerat, justo nec semper consectetur, justo eros accumsan risus, eget consequat nisl metus id enim. Donec sapien urna, imperdiet sed elit quis, aliquam lacinia dolor. Vestibulum purus tortor, facilisis vitae cursus eget, condimentum non augue. Mauris pharetra, lectus viverra maximus venenatis, odio mauris suscipit diam, eget tempor nisi nibh congue magna. Nam ac facilisis odio, vel elementum quam. Morbi eros ligula, bibendum at ex a, ornare volutpat lacus. Duis leo tellus, vehicula et gravida quis, ultrices et nisi. Morbi diam mi, dignissim at lacus eu, venenatis eleifend urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Nulla facilisi. Nulla orci nulla, faucibus id velit ac, maximus sodales justo. Aliquam condimentum lorem at sollicitudin maximus. Ut lacinia ipsum ante, vel lobortis sem vehicula eget. Morbi congue massa sed purus pellentesque tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In fermentum est quis vehicula volutpat. Aliquam erat volutpat.
+
+Aenean accumsan mollis dolor, eget fermentum mauris hendrerit lacinia. Morbi malesuada consequat rutrum. Etiam finibus vehicula enim, vel luctus diam tempus viverra. Nulla pretium tellus id ligula volutpat, imperdiet elementum metus sodales. Nulla gravida, mi eget ullamcorper vestibulum, purus lacus imperdiet lacus, vitae egestas quam sapien sed justo. Curabitur scelerisque et orci non pellentesque. Aenean pretium dui sed est mollis, vitae suscipit nunc porta. Nulla facilisi.</p>
+            </Col>
+        </Row>
+        <Row>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac laoreet nisl. Praesent id dignissim elit, non sollicitudin ante. Nullam efficitur turpis at urna imperdiet, eget tincidunt arcu varius. Nulla facilisi. Nulla pellentesque orci mauris, in mattis neque lacinia vel. Vestibulum malesuada risus ultricies est mattis, nec placerat enim sollicitudin. Quisque quis erat eu augue hendrerit varius. Pellentesque vitae consectetur lorem. Pellentesque at odio ac turpis venenatis egestas ac vitae eros. Vestibulum pellentesque laoreet luctus. Phasellus nec felis vitae diam varius pharetra vitae eget mauris. Nullam mollis erat lobortis nulla feugiat, vel feugiat velit bibendum. Sed turpis neque, dignissim sed interdum vel, gravida eu mauris. Nam dapibus elit tortor, quis elementum ex fringilla vel.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Vestibulum at tincidunt justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi in sagittis turpis, sed semper diam. Sed eu massa vitae tellus hendrerit iaculis vel vel augue. Pellentesque sed tortor sodales, suscipit lacus faucibus, faucibus tortor. Praesent quis scelerisque dui. Nunc tincidunt sem vitae sem finibus mollis. Nam at sapien sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis consectetur dapibus ligula, vel aliquet nibh consequat id. Nulla ac egestas leo, nec lacinia odio. Donec rhoncus est ac arcu gravida, volutpat congue dui rutrum. Sed a nunc ex. Aenean ullamcorper luctus varius. Aliquam mattis sem ut arcu laoreet, vel faucibus leo facilisis.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Cras ut finibus eros. Mauris arcu velit, iaculis in nibh vel, lacinia elementum nisl. Curabitur interdum arcu non enim hendrerit, sit amet placerat diam condimentum. Aliquam nec eros et ligula scelerisque bibendum. Nulla accumsan tincidunt elit cursus tincidunt. Sed ut dolor quis lorem hendrerit eleifend a sit amet nibh. Phasellus placerat, justo nec semper consectetur, justo eros accumsan risus, eget consequat nisl metus id enim. Donec sapien urna, imperdiet sed elit quis, aliquam lacinia dolor. Vestibulum purus tortor, facilisis vitae cursus eget, condimentum non augue. Mauris pharetra, lectus viverra maximus venenatis, odio mauris suscipit diam, eget tempor nisi nibh congue magna. Nam ac facilisis odio, vel elementum quam. Morbi eros ligula, bibendum at ex a, ornare volutpat lacus. Duis leo tellus, vehicula et gravida quis, ultrices et nisi. Morbi diam mi, dignissim at lacus eu, venenatis eleifend urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Nulla facilisi. Nulla orci nulla, faucibus id velit ac, maximus sodales justo. Aliquam condimentum lorem at sollicitudin maximus. Ut lacinia ipsum ante, vel lobortis sem vehicula eget. Morbi congue massa sed purus pellentesque tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In fermentum est quis vehicula volutpat. Aliquam erat volutpat.
+
+Aenean accumsan mollis dolor, eget fermentum mauris hendrerit lacinia. Morbi malesuada consequat rutrum. Etiam finibus vehicula enim, vel luctus diam tempus viverra. Nulla pretium tellus id ligula volutpat, imperdiet elementum metus sodales. Nulla gravida, mi eget ullamcorper vestibulum, purus lacus imperdiet lacus, vitae egestas quam sapien sed justo. Curabitur scelerisque et orci non pellentesque. Aenean pretium dui sed est mollis, vitae suscipit nunc porta. Nulla facilisi.</p>
+            </Col>
+        </Row>
+        <Row>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac laoreet nisl. Praesent id dignissim elit, non sollicitudin ante. Nullam efficitur turpis at urna imperdiet, eget tincidunt arcu varius. Nulla facilisi. Nulla pellentesque orci mauris, in mattis neque lacinia vel. Vestibulum malesuada risus ultricies est mattis, nec placerat enim sollicitudin. Quisque quis erat eu augue hendrerit varius. Pellentesque vitae consectetur lorem. Pellentesque at odio ac turpis venenatis egestas ac vitae eros. Vestibulum pellentesque laoreet luctus. Phasellus nec felis vitae diam varius pharetra vitae eget mauris. Nullam mollis erat lobortis nulla feugiat, vel feugiat velit bibendum. Sed turpis neque, dignissim sed interdum vel, gravida eu mauris. Nam dapibus elit tortor, quis elementum ex fringilla vel.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Vestibulum at tincidunt justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi in sagittis turpis, sed semper diam. Sed eu massa vitae tellus hendrerit iaculis vel vel augue. Pellentesque sed tortor sodales, suscipit lacus faucibus, faucibus tortor. Praesent quis scelerisque dui. Nunc tincidunt sem vitae sem finibus mollis. Nam at sapien sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis consectetur dapibus ligula, vel aliquet nibh consequat id. Nulla ac egestas leo, nec lacinia odio. Donec rhoncus est ac arcu gravida, volutpat congue dui rutrum. Sed a nunc ex. Aenean ullamcorper luctus varius. Aliquam mattis sem ut arcu laoreet, vel faucibus leo facilisis.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Cras ut finibus eros. Mauris arcu velit, iaculis in nibh vel, lacinia elementum nisl. Curabitur interdum arcu non enim hendrerit, sit amet placerat diam condimentum. Aliquam nec eros et ligula scelerisque bibendum. Nulla accumsan tincidunt elit cursus tincidunt. Sed ut dolor quis lorem hendrerit eleifend a sit amet nibh. Phasellus placerat, justo nec semper consectetur, justo eros accumsan risus, eget consequat nisl metus id enim. Donec sapien urna, imperdiet sed elit quis, aliquam lacinia dolor. Vestibulum purus tortor, facilisis vitae cursus eget, condimentum non augue. Mauris pharetra, lectus viverra maximus venenatis, odio mauris suscipit diam, eget tempor nisi nibh congue magna. Nam ac facilisis odio, vel elementum quam. Morbi eros ligula, bibendum at ex a, ornare volutpat lacus. Duis leo tellus, vehicula et gravida quis, ultrices et nisi. Morbi diam mi, dignissim at lacus eu, venenatis eleifend urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            </Col>
+            <Col
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+            >
+               <p style={{textAlign:'justify'}}>Nulla facilisi. Nulla orci nulla, faucibus id velit ac, maximus sodales justo. Aliquam condimentum lorem at sollicitudin maximus. Ut lacinia ipsum ante, vel lobortis sem vehicula eget. Morbi congue massa sed purus pellentesque tempor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In fermentum est quis vehicula volutpat. Aliquam erat volutpat.
+
+Aenean accumsan mollis dolor, eget fermentum mauris hendrerit lacinia. Morbi malesuada consequat rutrum. Etiam finibus vehicula enim, vel luctus diam tempus viverra. Nulla pretium tellus id ligula volutpat, imperdiet elementum metus sodales. Nulla gravida, mi eget ullamcorper vestibulum, purus lacus imperdiet lacus, vitae egestas quam sapien sed justo. Curabitur scelerisque et orci non pellentesque. Aenean pretium dui sed est mollis, vitae suscipit nunc porta. Nulla facilisi.</p>
+            </Col>
+        </Row>
+    </Container>
     </>)
 }
 export default Test;
