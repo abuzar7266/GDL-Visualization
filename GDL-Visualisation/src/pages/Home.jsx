af76f95016239a3a9b72c1e6f2829b5c06e000e3
abuzar7266 <i190531@nu.edu.pk>
2022-10-09 10:09:06 +0500

    Update-2

--- GDL-Visualisation/src/pages/Home.jsx
+++ GDL-Visualisation/src/pages/Home.jsx
@@ -7,10 +7,8 @@ import { useNavigate } from "react-router-dom";
 import * as d3 from 'd3';
 import { useRef } from "react";
 import { useEffect } from "react";
-<<<<<<< HEAD
 import Graph from "../component/Graph.jsx"
 import lesMis from "../assets/json/miserables.js"
-=======
 const LandingPage = ()=>{
     const navigate = useNavigate();
     return (<>
@@ -27,12 +25,11 @@ const LandingPage = ()=>{
         <Row>
             <Col>
                 <div className="button-div" style={{zIndex:'3'}}>
-                    <button className="Start-Button" onClick={() => navigate("/intro")}>Get Started</button>
+                    <button className="Start-Button" onClick={() => navigate("/vis")}>Get Started</button>
                 </div>
             </Col>
         </Row>
         </div>
-<<<<<<< HEAD
         <div  style={{zIndex:'1', position:'absolute'}} class="svg-container">
             <Row>
                 <Col>
@@ -40,119 +37,7 @@ const LandingPage = ()=>{
                 </Col>
             </Row>
         </div>
-        {/* <div>
-=======
-        <div  style={{zIndex:'2', position:'absolute'}}>
     </Container>
-    {/*<div className='easeIntroIn'>
-        <Container style={{textAlign:'center',marginTop:'12%'}} fluid>
-            <Row>
-                <Col
-                sm={1}
-                >
-                
-                </Col>
-                <Col
-                sm={8}
-                >
-                <div className="card-intro">
-                    <p className="start-intro">Our website provides series of algoirthmic visualisation related to geometric deep learning. We have developed a story that demonstrates the all the traditional algorithms previously in animated visualisation</p>
-                </div>
-                </Col>
-            </Row>
-        </Container>
-    </div>*/}
-    </>)
-}
-export default LandingPage;
-<<<<<<< HEAD
-import Graph from "../component/Graph.jsx"
-import lesMis from "../assets/json/miserables.js"
-=======
-const LandingPage = ()=>{
-    const navigate = useNavigate();
-    return (<>
-    <Container style={{textAlign:'left'}}>
-        <div  style={{zIndex:'3', position:'absolute'}}>
-        <Row>
-            <Col>
-                <div className="title-div">
-                    <span className="title">FYP Name</span><br />
-                    <span className="subtitle">Algorithmic Visualisation of Geometric Deep Learning</span>
-                </div>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <div className="button-div" style={{zIndex:'3'}}>
-                    <button className="Start-Button" onClick={() => navigate("/intro")}>Get Started</button>
-                </div>
-            </Col>
-        </Row>
-        </div>
-<<<<<<< HEAD
-        <div  style={{zIndex:'1', position:'absolute'}} class="svg-container">
-            <Row>
-                <Col>
-                    <Graph id="HomeGraph" strength={-90} height={1000} width={1800} data={lesMis}/>
-                </Col>
-            </Row>
-        </div>
-        {/* <div>
-=======
-        <div  style={{zIndex:'2', position:'absolute'}}>
-    </Container>
-    {/*<div className='easeIntroIn'>
-        <Container style={{textAlign:'center',marginTop:'12%'}} fluid>
-            <Row>
-                <Col
-                sm={1}
-                >
-                
-                </Col>
-                <Col
-                sm={8}
-                >
-                <div className="card-intro">
-                    <p className="start-intro">Our website provides series of algoirthmic visualisation related to geometric deep learning. We have developed a story that demonstrates the all the traditional algorithms previously in animated visualisation</p>
-                </div>
-                </Col>
-            </Row>
-        </Container>
-    </div>*/}
-    </>)
-}
-export default LandingPage;
-<<<<<<< HEAD
-        <div  style={{zIndex:'1', position:'absolute'}} class="svg-container">
-            <Row>
-                <Col>
-                    <Graph id="HomeGraph" strength={-90} height={1000} width={1800} data={lesMis}/>
-                </Col>
-            </Row>
-        </div>
-        {/* <div>
-=======
-        <div  style={{zIndex:'2', position:'absolute'}}>
-    </Container>
-    {/*<div className='easeIntroIn'>
-        <Container style={{textAlign:'center',marginTop:'12%'}} fluid>
-            <Row>
-                <Col
-                sm={1}
-                >
-                
-                </Col>
-                <Col
-                sm={8}
-                >
-                <div className="card-intro">
-                    <p className="start-intro">Our website provides series of algoirthmic visualisation related to geometric deep learning. We have developed a story that demonstrates the all the traditional algorithms previously in animated visualisation</p>
-                </div>
-                </Col>
-            </Row>
-        </Container>
-    </div>*/}
     </>)
 }
 export default LandingPage;
